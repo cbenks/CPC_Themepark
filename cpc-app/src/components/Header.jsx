7de44da78aae21c513b9f91fdbc0fe8ffa043ac9
Cory <cjbenicak@gmail.com>
2022-07-21 12:36:06 -0400

    Created Client Structure

---
+++ cpc-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
