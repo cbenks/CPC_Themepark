55722512250be722eafa10d421a8041490f8a726
Hyeree Park <hp2240@nyu.edu>
2022-07-21 21:00:37 -0400

    working on the landing page

--- cpc-app/src/pages/Home.js
+++ cpc-app/src/pages/Home.js
@@ -1,5 +1,46 @@
 const Home = () => {
-  return <div></div>
+  return (
+    <div className="slideshow-container">
+      <div className="mySlides fade">
+        <div className="numbertext">1 / 4</div>
+        <img src="" />
+        <div className="text">Caption Text</div>
+      </div>
+
+      {/* <div class="mySlides fade">
+        <div class="numbertext">2 / 4</div>
+        <img
+          src="images/hotel%20background1.png"
+          alt="Hotel Background"
+          style="width:100%"
+          height="450"
+        />
+        <div class="text">Caption Two</div>
+      </div>
+
+      <div class="mySlides fade">
+        <div class="numbertext">3 / 4</div>
+        <img
+          src="images/hotel%20background2.png"
+          alt="Hotel Background"
+          style="width:100%"
+          height="450"
+        />
+        <div class="text">Caption Three</div>
+      </div>
+
+      <div class="mySlides fade">
+        <div class="numbertext">4 / 4</div>
+        <img
+          src="images/hotel%20background3.png"
+          alt="Hotel Background"
+          style="width:100%"
+          height="450"
+        />
+        <div class="text">Caption Three</div>
+      </div> */}
+    </div>
+  )
 }
 
 export default Home
