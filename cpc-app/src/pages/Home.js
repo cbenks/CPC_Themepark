b9382cc82049359c6398491d62e17c3f1c80f998
Hyeree Park <hp2240@nyu.edu>
2022-07-22 15:35:59 -0400

    final push before presentation

--- cpc-app/src/pages/Home.js
+++ cpc-app/src/pages/Home.js
@@ -2,15 +2,7 @@ import img from '../assets/themePark1.png'
 console.log(img)
 
 const Home = () => {
-  return (
-    // <div className="slideshow-container">
-    //   <div className="mySlides fade">
-    //     <div className="numbertext">1 / 4</div>
-    <img src={img} />
-    //     <div className="text">Caption Text</div>
-    //   </div>
-    // </div>
-  )
+  return <img src={img} />
 }
 
 export default Home
